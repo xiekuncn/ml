66663fdf68a2b51cd2d6a2c04f2e213c0f89b7ad
xiekun <xiekun@cvte.com>
2016-01-06 17:31:32 +0800

    [ex1]finished the base exersise 1.

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,15 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    theta = theta - (alpha ./ m) .* sum(repmat((X * theta - y), [1, 2]) .* X)';
     % ============================================================
-
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
